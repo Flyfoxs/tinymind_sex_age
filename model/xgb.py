33fb0aed7894344df4eb0903615a44e31beda496
lali2 <lali2@cisco.com>
2018-09-14 20:31:19 +0800

    LGBMClassifier initial

--- model/xgb.py
+++ model/xgb.py
@@ -1,7 +1,9 @@
-from lightgbm.sklearn import LGBMRegressor, LGBMClassifier
-from xgboost.sklearn import XGBRegressor
-import lightgbm as lgb
-from sklearn.cross_validation import train_test_split
+
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+
+from xgboost import XGBClassifier
 
 from tiny.lda import *
 from  tiny.util import *
@@ -9,29 +11,15 @@ from  tiny.util import *
 # New add
 # deviceid_train.rename({'device_id':'device'}, axis=1, inplace=True)
 deviceid_train = get_lda_feature()
-deviceid_train2 = get_lda_from_usage(mini=mini)
-
-core_list = ['0', '1', '2', '3','4']
-# for col in core_list:
-#     deviceid_train_2[col] =  deviceid_train_2[col].apply(lambda val: 1 if val > 0 else 0)
-deviceid_train = pd.concat([deviceid_train,deviceid_train2[core_list] ], axis=1)
-
 deviceid_train = extend_feature(span_no=24, input=deviceid_train, trunc_long_time=False)
-
-#deviceid_train = extend_feature(span_no=4,input=deviceid_train,  trunc_long_time=False)
-
+#extend_feature(span_no=6, input=deviceid_train, trunc_long_time=900)
 
 
-#print(len(deviceid_train))
-#deviceid_train.groupby('max_day_cnt')['max_day_cnt'].count()
 
-#
-#
-# col_drop = [item for item in deviceid_train.columns if 'max_' in str(item)]
-# deviceid_train.drop(columns=col_drop, inplace=True )
 
-#deviceid_train.drop(columns=['tfidf_sum'], inplace=True )
-deviceid_train.head()
+col_drop = [item for item in deviceid_train.columns if 'max_' in str(item)]
+deviceid_train.drop(columns=col_drop, inplace=True )
+
 
 
 train=deviceid_train[deviceid_train['sex'].notnull()]
@@ -39,53 +27,59 @@ test=deviceid_train[deviceid_train['sex'].isnull()]
 
 X = train.drop(['sex', 'age', 'sex_age', 'device'], axis=1)
 Y = train['sex_age']
-Y_CAT = pd.Categorical(Y)
-X_train, X_test, y_train, y_test = train_test_split(X, Y_CAT.labels, test_size=0.3, random_state=666)
-# lgb_train = lgb.Dataset(X_train, label=y_train)
-# lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
+# Y_CAT = pd.Categorical(Y)
+# X_train, X_test, y_train, y_test = train_test_split(X, Y_CAT.codes, test_size=0.3, random_state=666)
+
+xgb = XGBClassifier(learning_rate=0.02, n_estimators=600, objective='multi:softprob',
+                    silent=True, nthread=1)
+
+
+
+folds = 3
+param_comb = 5
+
+skf = StratifiedKFold(n_splits=folds, shuffle = True, random_state = 1001)
+
 params = {
-    'boosting_type': 'gbdt',
-    'max_depth': 3,
-    'metric': {'multi_logloss'},
-    'num_class': 22,
-    'objective': 'multiclass',
-    'random_state': 47,
+        'min_child_weight': [1, 5, 10],
+        'gamma': [0.5, 1, 1.5, 2, 5],
+        'subsample': [0.6, 0.8, 1.0],
+        'colsample_bytree': [0.6, 0.8, 1.0],
+        'max_depth': [3, 4, 5]
+        }
 
-    "min_data_in_leaf":1000,
-    'verbose': -1
+random_search = RandomizedSearchCV(xgb, param_distributions=params,
+                                   n_iter=param_comb, scoring='neg_log_loss', n_jobs=4,
+                                   cv=skf.split(X,Y), verbose=3, random_state=1001 )
 
-}
+# print(random_search.grid_scores_)
+random_search.fit(X, Y)
+# print(random_search.grid_scores_)
 
+print('\n All results:')
+print(random_search.cv_results_)
+print('\n Best estimator:')
+print(random_search.best_estimator_)
+print('\n Best normalized gini score for %d-fold search with %d parameter combinations:' % (folds, param_comb))
+print(random_search.best_score_ * 2 - 1)
+print('\n Best hyperparameters:')
+print(random_search.best_params_)
+results = pd.DataFrame(random_search.cv_results_)
 
-gbm = LGBMClassifier(n_estimators=1000,)
+# bst = xgb.train(params, dtrain, 50, watchlist );
 
-gbm.set_params(**params)
+#
+# pred = bst.predict(xgb.DMatrix(X_test) );
+# print(pred[:2])
+#
+# print(xgb.plot_importance(bst))
+# print ('predicting, classification error=%f' % (sum( int(pred[i]) != test_Y[i] for i in range(len(test_Y))) / float(len(test_Y)) ))
+# do the same thing again, but output probabilities
 
-print(gbm)
-
-gbm.fit(X_train, y_train, eval_set=[(X_test, y_test)],early_stopping_rounds=100,)
-
-#y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)#
-print('Feature importances:', list(gbm.feature_importances_))
-
-best = round(gbm.best_score_.get('valid_0').get('multi_logloss'), 5)
-best
-
-
-pre_x=test.drop(['sex','age','sex_age','device'],axis=1)
-sub=pd.DataFrame(gbm.predict_proba(pre_x.values,num_iteration=gbm.best_iteration_))
-
-
-sub.columns=Y_CAT.categories
-sub['DeviceID']=test['device'].values
-sub=sub[['DeviceID', '1-0', '1-1', '1-2', '1-3', '1-4', '1-5', '1-6', '1-7','1-8', '1-9', '1-10', '2-0', '2-1', '2-2', '2-3', '2-4', '2-5', '2-6', '2-7', '2-8', '2-9', '2-10']]
-
-file = f'./sub/baseline_{best}.csv'
-print(f'sub file save to {file}')
-sub.to_csv(file,index=False)
-
-#lgb.plot_importance(gbm, max_num_features=20)
-
-print(f'=============\nFinal train feature:{list(deviceid_train.columns)}')
-
-
+# params['objective'] = 'multi:softprob'
+# bst = xgb.train(params, dtrain, 5, watchlist );
+# Note: this convention has been changed since xgboost-unity
+# get prediction, this is in 1D array, need reshape to (ndata, nclass)
+# yprob = bst.predict( X_test ).reshape( test_Y.shape[0], 6 )
+# ylabel = np.argmax(yprob, axis=1)
+# print ('predicting, classification error=%f' % (sum( int(ylabel[i]) != test_Y[i] for i in range(len(test_Y))) / float(len(test_Y)) ))
