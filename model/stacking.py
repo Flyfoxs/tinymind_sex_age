ee54d6dd779e3253d1721293c9fefee8b2cecb74
lali2 <lali2@cisco.com>
2018-09-29 14:19:40 +0800

    ./sub/baseline_st_2.8576921724124866.csv

--- model/stacking.py
+++ model/stacking.py
@@ -7,70 +7,118 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from mlxtend.classifier import StackingClassifier, StackingCVClassifier
 import numpy as np
+from xgboost import XGBClassifier
+
 from tiny.util import *
 
-feature_label = get_stable_feature()
+try:
+    from tiny.conf import gpu_params
+except :
+    # GPU support
+    gpu_params = {}
+
+feature_label = get_stable_feature('0924')
 
 
 
 train=feature_label[feature_label['sex'].notnull()]
 test =feature_label[feature_label['sex'].isnull()]
 
-X = train.drop(['sex', 'age', 'sex_age', 'device'], axis=1)
-
-
-
-Y = train['sex_age']
+X = train.drop(['sex', 'age', 'sex_age', 'device'], axis=1)[:10000]
+Y = train['sex_age'][:10000]
 Y_CAT = pd.Categorical(Y)
 #X_train, X_test, y_train, y_test = train_test_split(X, Y_CAT.labels, test_size=0.3, random_state=666)
 
 
 
-lgbm =  LGBMClassifier(n_estimators=1000,
-                     boosting_type='gbdt',
-                     objective='multiclass',
-                     num_class=22,
-                     random_state=47,
-                     eval_metric='logloss',
-                     subsample=0.5,
-                     feature_fraction=0.2,
-                     reg_alpha=3,
-                     reg_lambda=4,
-                     min_data_in_leaf=1472,
-                     max_depth= 3,
-                     verbose=-1,
-                    )
-rf = RandomForestClassifier(n_estimators=5000,
-                                        #criterion='entropy',
-                                        verbose=1,
-                                        n_jobs=-1,
-                                        random_state=42)
+lgbm =  LGBMClassifier(n_estimators=5000,
+                         boosting_type='gbdt',
+                         objective='multiclass',
+                         num_class=22,
+                         random_state=47,
+                         metric=['multi_logloss'],
+                         verbose=-1,
+                         max_depth=3,
 
-gnb = GaussianNB()
+
+                         feature_fraction=0.2,
+                         subsample=0.5,
+                         min_data_in_leaf=1472,
+
+                         reg_alpha=2,
+                         reg_lambda=4,
+
+                         ##########
+                         learning_rate=0.02,  # 0.1
+                         colsample_bytree=None,  #1
+                         min_child_samples=None,  #20
+                         min_child_weight=None,  #0.001
+                         min_split_gain=None,  #0
+                         num_leaves=None,  #31
+                         subsample_for_bin=None,  #200000
+                         subsample_freq=None,  #1
+                         nthread=-1,
+
+                         )
+
+rf = ExtraTreesClassifier(n_estimators=1000,
+                                      max_depth=15,
+                                      max_features=128,
+                                      verbose=1,
+                                      n_jobs=-1,
+                                      random_state=42)
+
+xgb = XGBClassifier(
+                    objective='multi:softprob',
+                    eval_metric='mlogloss',
+                    num_class=22,
+                    max_depth=3,
+                    reg_alpha=10,
+                    reg_lambda=10,
+                    subsample=0.7,
+                    colsample_bytree=0.6,
+                    n_estimators=2000,
+
+
+                    learning_rate=0.01,
+
+
+                    seed=1,
+                    missing=None,
+
+                    #Useless Paras
+                    silent=True,
+                    gamma=0,
+                    max_delta_step=0,
+                    min_child_weight=1,
+                    colsample_bylevel=1,
+                    scale_pos_weight=1,
+
+                    **gpu_params
+                    )
+
 
 lr = LogisticRegression()
-sclf = StackingCVClassifier(classifiers=[lgbm, rf, gnb],
+sclf = StackingCVClassifier(classifiers=[lgbm, xgb, rf],
                           #use_probas=True,
                          # average_probas=False,
                           meta_classifier=lr)
 
-#sclf = lgbm
-
-sclf = gnb
 
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y_CAT.labels, test_size=0.3, random_state=666)
+X_train, X_test, y_train, y_test = train_test_split(X, Y_CAT.codes, test_size=0.3, random_state=666)
 
+logger.debug("Fix begin")
 sclf.fit(X_train.values, y_train)
-
+logger.debug("Fix End")
 
 # #train = feature_label[feature_label['sex'].notnull()]
 # test = feature_label[feature_label['sex'].isnull()]
 
 from sklearn.metrics import log_loss
 
-best = log_loss(y_test, sclf.predict_proba(X_test))
+best = log_loss(y_test, sclf.predict_proba(X_test.values))
 
 # lgb.plot_importance(gbm, max_num_features=20)
 
